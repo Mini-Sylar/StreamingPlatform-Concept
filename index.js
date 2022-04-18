a7f0f583a4694131f9199f002d3dd2c09d674598
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-04-18 21:29:18 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const mobile_nav = document.querySelector(".links-mobile");
+const ham = document.querySelector(".ham");
+const body = document.querySelector("body")
+ham.addEventListener("click", () => {
+  mobile_nav.classList.toggle("show-links");
+  if (mobile_nav.classList.contains("show-links")) {
+    body.style=`
+        overflow:hidden;
+    `
+  }
+  else{
+       body.style = `
+        overflow:scroll;
+    `;
+  }
+});
